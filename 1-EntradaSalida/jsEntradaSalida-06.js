7202139ac66d442934a6ead05519a967fb902e22
GastonCragno <gastonemanuelcragno@gmail.com>
2022-02-02 18:44:32 -0300

    ejercicios

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,25 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	//propongo las variables
+	var primernumero
+	var segundonumero
+	var resultado
+
+
+	//Paso a enteros las variables
+
+	primernumero = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	segundonumero = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+
+	//le doy el valor al resultado mediante la suma de las variables
+
+	resultado = primernumero + segundonumero;
+
+	//muestro el resultado de la suma
+	alert("la suma es "+resultado);
+
 }
 
