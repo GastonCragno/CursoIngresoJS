f343449304bac4117508bed0ad785f1518473de5
GastonCragno <gastonemanuelcragno@gmail.com>
2022-02-02 16:47:39 -0300

    subiendo archivos a gh

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,13 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//ponemos una variable
+	var nombreingresado;
+	
+	//le pedimos a la persona que ingrese su nombre
+	nombreingresado=prompt("ingrese su nombre") ;
+	
+	//le informamos y mostramos a la persona cual es su nombre
+	alert("Su nombre es: "+nombreingresado)
 }
 
